8a89b8e923c1d36d6a2eea25ace07539b3db1650
YvesZHI <yuhui.zhi01@gmail.com>
2018-01-10 17:56:12 +0800

    process empty string for val

--- processType.sh
+++ processType.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 
 val=$(grep '(\*write)' $1 | awk '{print $1}')
 
+val=''
+
+if [[ $val == "" ]]; then
+	echo 'aaaaaaaaaaaaaaaaaaaaaaaa'
+	val='ssize_t'
+fi
 
 sed -i -e "s/static .* hi1616_dfx_write/static ${val} hi1616_dfx_write/g" ./proc/proc.c
 
